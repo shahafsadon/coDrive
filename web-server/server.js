6dbe75a470f16554a251b5ff7fe17e1e3eb6719d
Alon <alonco267@gmail.com>
2025-12-16 12:04:05 +0200

    SCRUM-298 TCP Client Design for C++ Server

--- web-server/server.js
+++ web-server/server.js
@@ -16,3 +16,17 @@ const errorHandler = require('./middleware/errorMiddleware');
 
 // must be AFTER routes
 app.use(errorHandler);
+
+///////////////
+const cppClient = require('./services/cppServerClient');
+
+process.on('SIGINT', async () => {
+  await cppClient.close();
+  process.exit(0);
+});
+
+process.on('SIGTERM', async () => {
+  await cppClient.close();
+  process.exit(0);
+});
+////////////////
